9307c78ec6812b325f9b5df11bd9f3daf6981d33
Michael Williamson <mike@zwobble.org>
2011-06-18 21:38:25 +0100

    Add type checking for immutable variable declarations

--- src/org/zwobble/shed/compiler/typechecker/TypeChecker.java
+++ src/org/zwobble/shed/compiler/typechecker/TypeChecker.java
@@ -1,5 +1,67 @@
 package org.zwobble.shed.compiler.typechecker;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.zwobble.shed.compiler.parsing.CompilerError;
+import org.zwobble.shed.compiler.parsing.nodes.ImmutableVariableNode;
+import org.zwobble.shed.compiler.parsing.nodes.SourceNode;
+import org.zwobble.shed.compiler.parsing.nodes.StatementNode;
+import org.zwobble.shed.compiler.types.Type;
+
+import static org.zwobble.shed.compiler.typechecker.TypeResult.failure;
+import static org.zwobble.shed.compiler.typechecker.TypeResult.success;
+
 public class TypeChecker {
-    
+    private final SimpleNodeLocations nodeLocations;
+    private final TypeLookup typeLookup;
+    private final TypeInferer typeInferer;
+
+    public TypeChecker(SimpleNodeLocations nodeLocations, TypeLookup typeLookup, TypeInferer typeInferer) {
+        this.nodeLocations = nodeLocations;
+        this.typeLookup = typeLookup;
+        this.typeInferer = typeInferer;
+    }
+
+    public TypeResult<Void> typeCheck(SourceNode source, StaticContext staticContext) {
+        List<CompilerError> errors = new ArrayList<CompilerError>();
+        for (StatementNode statement : source.getStatements()) {
+            TypeResult<Void> result = typeCheckStatement(statement, staticContext);
+            errors.addAll(result.getErrors());
+        }
+        if (errors.isEmpty()) {
+            return success(null);
+        } else {
+            return failure(errors);
+        }
+    }
+
+    private TypeResult<Void> typeCheckStatement(StatementNode statement, StaticContext staticContext) {
+        List<CompilerError> errors = new ArrayList<CompilerError>();
+        if (statement instanceof ImmutableVariableNode) {
+            ImmutableVariableNode immutableVariable = (ImmutableVariableNode)statement;
+            
+            TypeResult<Type> valueTypeResult = typeInferer.inferType(immutableVariable.getValue(), staticContext);
+            errors.addAll(valueTypeResult.getErrors());
+            
+            if (immutableVariable.getTypeReference().hasValue()) {
+                TypeResult<Type> typeResult = typeLookup.lookupTypeReference(immutableVariable.getTypeReference().get(), staticContext);
+                errors.addAll(typeResult.getErrors());
+                if (errors.isEmpty() && !valueTypeResult.get().equals(typeResult.get())) {
+                    errors.add(new CompilerError(
+                        nodeLocations.locate(statement),
+                        "Cannot initialise variable of type \"" + typeResult.get().shortName() +
+                            "\" with expression of type \"" + valueTypeResult.get().shortName() + "\"))"
+                    ));
+                }
+            }
+            
+            if (errors.isEmpty()) {
+                return success(null);
+            } else {
+                return failure(errors);
+            }
+        }
+        throw new RuntimeException("Cannot check type of statement: " + statement);
+    }
 }
---
+++ test/org/zwobble/shed/compiler/typechecker/TypeCheckerTest.java
@@ -0,0 +1,69 @@
+package org.zwobble.shed.compiler.typechecker;
+
+import java.util.Collections;
+
+import org.junit.Test;
+import org.zwobble.shed.compiler.parsing.CompilerError;
+import org.zwobble.shed.compiler.parsing.nodes.BooleanLiteralNode;
+import org.zwobble.shed.compiler.parsing.nodes.ImmutableVariableNode;
+import org.zwobble.shed.compiler.parsing.nodes.ImportNode;
+import org.zwobble.shed.compiler.parsing.nodes.PackageDeclarationNode;
+import org.zwobble.shed.compiler.parsing.nodes.PublicDeclarationNode;
+import org.zwobble.shed.compiler.parsing.nodes.SourceNode;
+import org.zwobble.shed.compiler.parsing.nodes.StatementNode;
+import org.zwobble.shed.compiler.parsing.nodes.TypeIdentifierNode;
+import org.zwobble.shed.compiler.parsing.nodes.TypeReferenceNode;
+import org.zwobble.shed.compiler.types.CoreTypes;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.zwobble.shed.compiler.Option.none;
+import static org.zwobble.shed.compiler.Option.some;
+import static org.zwobble.shed.compiler.parsing.SourcePosition.position;
+import static org.zwobble.shed.compiler.parsing.SourceRange.range;
+
+public class TypeCheckerTest {
+    private final SimpleNodeLocations nodeLocations = new SimpleNodeLocations();
+    private final TypeLookup typeLookup = new TypeLookup(nodeLocations);
+    private final TypeInferer typeInferer = new TypeInferer(nodeLocations, typeLookup);
+    private final TypeChecker typeChecker = new TypeChecker(nodeLocations, typeLookup, typeInferer);
+    
+    private final StaticContext staticContext = new StaticContext();
+    
+    @Test public void
+    noErrorsIfEverythingTypeChecks() {
+        SourceNode source = new SourceNode(
+            new PackageDeclarationNode(asList("shed", "example")),
+            Collections.<ImportNode>emptyList(),
+            new PublicDeclarationNode(asList("x")),
+            asList((StatementNode)new ImmutableVariableNode("x", none(TypeReferenceNode.class), new BooleanLiteralNode(true)))
+        );
+        assertThat(typeChecker.typeCheck(source, staticContext).isSuccess(), is(true));
+    }
+    
+    @Test public void
+    errorsIfAttemptingToInitialiseAVariableWithExpressionOfWrongType() {
+        StatementNode variableNode = new ImmutableVariableNode(
+            "x",
+            some((TypeReferenceNode)new TypeIdentifierNode("String")),
+            new BooleanLiteralNode(true)
+        );
+        nodeLocations.put(variableNode, range(position(4, 12), position(6, 6)));
+        
+        SourceNode source = new SourceNode(
+            new PackageDeclarationNode(asList("shed", "example")),
+            Collections.<ImportNode>emptyList(),
+            new PublicDeclarationNode(asList("x")),
+            asList(variableNode)
+        );
+        staticContext.add("String", CoreTypes.classOf(CoreTypes.STRING));
+        assertThat(
+            typeChecker.typeCheck(source, staticContext),
+            is(TypeResult.<Void>failure(asList(new CompilerError(
+                range(position(4, 12), position(6, 6)),
+                "Cannot initialise variable of type \"String\" with expression of type \"Boolean\"))"
+            ))))
+        );
+    }
+}
