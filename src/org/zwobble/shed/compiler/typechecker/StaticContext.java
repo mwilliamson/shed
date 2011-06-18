a4fb1c89f6395f22ab39c284b7367828aa6f4a22
Michael Williamson <mike@zwobble.org>
2011-06-18 21:59:37 +0100

    Add default static context

--- src/org/zwobble/shed/compiler/typechecker/StaticContext.java
+++ src/org/zwobble/shed/compiler/typechecker/StaticContext.java
@@ -4,9 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.zwobble.shed.compiler.Option;
+import org.zwobble.shed.compiler.types.CoreTypes;
 import org.zwobble.shed.compiler.types.Type;
 
 public class StaticContext {
+    public static StaticContext defaultContext() {
+        StaticContext staticContext = new StaticContext();
+        staticContext.add("String", CoreTypes.classOf(CoreTypes.STRING));
+        staticContext.add("Number", CoreTypes.classOf(CoreTypes.NUMBER));
+        staticContext.add("Boolean", CoreTypes.classOf(CoreTypes.BOOLEAN));
+        staticContext.add("Class", CoreTypes.classOf(CoreTypes.CLASS));
+        return staticContext;
+    }
+    
     private final Map<String, Type> values = new HashMap<String, Type>();
     
     public void add(String identifier, Type type) {
