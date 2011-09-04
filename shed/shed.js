f816f95edeab835e47f6cd025a2d7b3c5102c3f2
Michael Williamson <mike@zwobble.org>
2011-09-04 11:34:41 +0100

    Split out core values into a separate module

--- shed/shed.js
+++ shed/shed.js
@@ -1,20 +1,4 @@
 (function(exports) {
-    var unit = {};
-    var identityFunction = function(value) {
-        return value;
-    };
-    exports.String = identityFunction;
-    exports.Boolean = identityFunction;
-    exports.Number = identityFunction;
-    exports.Unit = function() {
-        return unit;
-    };
-    for (var i = 0; i < 20; i += 1) {
-        exports["Function" + i] = function() {
-            return {};
-        };
-    }
-    
     exports.export = function(name, value) {
         var parts = name.split(".");
         var current = exports;
---
+++ shed/shed/core.browser.js
@@ -0,0 +1,20 @@
+(function() {
+    var exports = {};
+    var unit = {};
+    var identityFunction = function(value) {
+        return value;
+    };
+    exports.String = identityFunction;
+    exports.Boolean = identityFunction;
+    exports.Number = identityFunction;
+    exports.Unit = function() {
+        return unit;
+    };
+    for (var i = 0; i < 20; i += 1) {
+        exports["Function" + i] = function() {
+            return {};
+        };
+    }
+    
+    SHED.export("shed.core", exports);
+})();
