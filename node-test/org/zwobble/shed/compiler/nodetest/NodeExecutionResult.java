6cf34a469c61d5dfb1c53edbd02e93758bc49dde
Michael Williamson <mike@zwobble.org>
2011-10-21 22:15:02 +0100

    Adding test using node as a target

---
+++ node-test/org/zwobble/shed/compiler/nodetest/NodeExecutionResult.java
@@ -0,0 +1,9 @@
+package org.zwobble.shed.compiler.nodetest;
+
+import lombok.Data;
+
+@Data
+public class NodeExecutionResult {
+    private final int returnCode;
+    private final String output;
+}
---
+++ node-test/org/zwobble/shed/compiler/nodetest/NodeTesting.java
@@ -0,0 +1,29 @@
+package org.zwobble.shed.compiler.nodetest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+public class NodeTesting {
+    public static Matcher<NodeExecutionResult> isSuccessWithOutput(final String expectedOutput) {
+        return new TypeSafeDiagnosingMatcher<NodeExecutionResult>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("success with output: " + expectedOutput);
+            }
+
+            @Override
+            protected boolean matchesSafely(NodeExecutionResult item, Description mismatchDescription) {
+                if (item.getReturnCode() != 0) {
+                    mismatchDescription.appendText("had return code " + item.getReturnCode());
+                    return false;
+                }
+                if (!item.getOutput().equals(expectedOutput)) {
+                    mismatchDescription.appendText("had output: " + item.getOutput());
+                    return false;
+                }
+                return true;
+            }
+        };
+    }
+}
