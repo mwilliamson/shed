c85173f90fb6f242633676e24000c351996f57e3
Michael Williamson <mike@zwobble.org>
2011-10-22 13:28:03 +0100

    Optimise node output

--- node-test/org/zwobble/shed/compiler/nodetest/NodeTests.java
+++ node-test/org/zwobble/shed/compiler/nodetest/NodeTests.java
@@ -1,14 +1,15 @@
 package org.zwobble.shed.compiler.nodetest;
 
+import java.io.CharArrayWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 import org.junit.Test;
+import org.zwobble.shed.compiler.GoogleClosureJavaScriptOptimiser;
 import org.zwobble.shed.compiler.nodejs.ShedToNodeJsCompiler;
 
 import com.google.common.io.CharStreams;
@@ -34,13 +35,16 @@ public class NodeTests {
 
     private File compile(String directory, String main) throws IOException {
         File compiledFile = File.createTempFile(main, ".js");
-        FileOutputStream outputStream = new FileOutputStream(compiledFile);
 
-        Writer writer = new OutputStreamWriter(outputStream);
+        Writer writer = new CharArrayWriter();
         writer.write(CharStreams.toString(new FileReader(new File("src/org/zwobble/shed/runtime/shed.node.js"))));
         ShedToNodeJsCompiler.compile(new File("src/org/zwobble/shed/runtime/stdlib"), main, writer);
         ShedToNodeJsCompiler.compile(new File("node-test-files", directory), main, writer);
         writer.flush();
+        FileWriter fileWriter = new FileWriter(compiledFile);
+        String optimised = new GoogleClosureJavaScriptOptimiser().optimise(writer.toString());
+        fileWriter.write(optimised);
+        fileWriter.flush();
         return compiledFile;
     }
 
