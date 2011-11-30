eba1961a7b13dac1b4f11d7490c4f6b8c02e39c6
Michael Williamson <mike@zwobble.org>
2011-11-30 22:07:58 +0000

    Create meta-classes on-demand

--- src/org/zwobble/shed/compiler/metaclassgeneration/MetaClasses.java
+++ src/org/zwobble/shed/compiler/metaclassgeneration/MetaClasses.java
@@ -1,20 +1,29 @@
 package org.zwobble.shed.compiler.metaclassgeneration;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import org.zwobble.shed.compiler.types.ClassType;
 import org.zwobble.shed.compiler.types.ScalarType;
 
 import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 
-@AllArgsConstructor(staticName="metaClasses")
+@RequiredArgsConstructor(staticName="metaClasses")
 public class MetaClasses {
-    private final BiMap<ScalarType, ClassType> typesToMetaClasses;
+    private final BiMap<ScalarType, ClassType> typesToMetaClasses = HashBiMap.<ScalarType, ClassType>create();
     
     public ClassType metaClassOf(ScalarType type) {
+        if (!typesToMetaClasses.containsKey(type)) {
+            typesToMetaClasses.put(type, createMetaClass(type));
+        }
         return typesToMetaClasses.get(type);
     }
+    
     public ScalarType getTypeFromMetaClass(ClassType metaClass) {
         return typesToMetaClasses.inverse().get(metaClass);
+    }
+    
+    private ClassType createMetaClass(ScalarType type) {
+        return new ClassType(type.getFullyQualifiedName().extend("$Meta"));
     }
 }
--- test/org/zwobble/shed/compiler/metaclassgeneration/MetaClassesTest.java
+++ test/org/zwobble/shed/compiler/metaclassgeneration/MetaClassesTest.java
@@ -4,33 +4,24 @@ import org.junit.Test;
 import org.zwobble.shed.compiler.types.ClassType;
 import org.zwobble.shed.compiler.types.ScalarType;
 
-import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.zwobble.shed.compiler.naming.FullyQualifiedName.fullyQualifiedName;
 import static org.zwobble.shed.compiler.types.TypeMatchers.classTypeWithName;
 
-public class MetaClassGeneratorTest {
+public class MetaClassesTest {
+    private final MetaClasses metaClasses = MetaClasses.metaClasses();
+    
     @Test public void
     metaClassesAreGeneratedForEachType() {
         ClassType type = new ClassType(fullyQualifiedName("shed", "Song"));
-        MetaClasses metaClasses = generate(types(type));
         assertThat(metaClasses.metaClassOf(type), is(classTypeWithName(fullyQualifiedName("shed", "Song", "$Meta"))));
     }
     
     @Test public void
     typeCanBeRetrivedByMetaClass() {
         ScalarType type = new ClassType(fullyQualifiedName("shed", "Song"));
-        MetaClasses metaClasses = generate(types(type));
         ClassType metaClass = metaClasses.metaClassOf(type);
         assertThat(metaClasses.getTypeFromMetaClass(metaClass), is(type));
-    }
-    
-    private Iterable<ScalarType> types(ScalarType... types) {
-        return asList(types);
-    }
-    
-    private MetaClasses generate(Iterable<ScalarType> types) {
-        return new MetaClassGenerator().generate(types);
     }
 }
